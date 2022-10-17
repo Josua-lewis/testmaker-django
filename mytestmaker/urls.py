a046ad5b517cd6661c19c635c9bf8597fcd32ac6
Josua Lewis <josualewis@outlook.com>
2022-10-17 13:01:27 +0200

    Added the path of the about_us.html in the urls.py file

--- mytestmaker/urls.py
+++ mytestmaker/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('signup.html', views.signup, name="signup"),
-    path('loginpage.html', views.loginpage, name="login")
+    path('loginpage.html', views.loginpage, name="login"),
+    path('about_us.html', views.about_us, name="about"),
+
 
 ]
