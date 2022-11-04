6fc7eb608abf659c9af481d8e9b44a3788e8f8b6
Josua Lewis <josualewis@outlook.com>
2022-11-04 13:24:32 +0200

    modified:   mytestmaker/__pycache__/urls.cpython-310.pyc 	modified:   mytestmaker/__pycache__/views.cpython-310.pyc 	modified:   mytestmaker/templates/about_us.html 	modified:   mytestmaker/templates/loginpage.html 	modified:   mytestmaker/urls.py 	modified:   mytestmaker/views.py

--- mytestmaker/urls.py
+++ mytestmaker/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('login/', views.loginpage, name="login"),
     path('about/', views.about_us, name="about"),
     path('contact/', views.contact_us, name="contact"),
+    path('reset/', views.reset_password, name="reset_password"),
+    path('forgot/', views.forgot_password, name="forgot_password"),
 
 ]
--- mytestmaker/views.py
+++ mytestmaker/views.py
@@ -16,3 +16,9 @@ def about_us(request):
 
 def contact_us(request):
     return render(request, 'contact_us.html', {})
+
+def reset_password(request):
+    return render(request, 'reset_password.html', {})
+
+def forgot_password(request):
+    return render(request, 'forgot_password.html', {})
