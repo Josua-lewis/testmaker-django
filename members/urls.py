7912b03ff670db862437485205182c5bd9f899d8
Josua Lewis <josualewis@outlook.com>
2022-11-09 12:40:34 +0200

    Added the logout_user path

--- members/urls.py
+++ members/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
    path('login_user', views.login_user, name="login"),
-   path('register', views.register, name="register"),
+   path('logout_user', views.logout_user, name="logout"),
+   path('register', views.register_user, name="register"),
 ]
